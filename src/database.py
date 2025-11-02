7400eedccc118cdea220fc4ec167dc851292e10c
CarloZ <carloz190405@gmail.com>
2025-11-02 16:52:12 -0600

    database code

---
+++ src/database.py
@@ -0,0 +1,25 @@
+import sqlite3 from .modelos import Tarea, Proyecto
+import os
+
+DATABASE_NAME = "tareas.db"
+
+def get_connection():
+    conn = sqlite3.connect(DATABASE_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def crear_tarea(self, Tarea: Tarea) -> Tarea:
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO tareas (titulo, descripcion, fecha_creacion, fecha limite, prioridad), estado, proyecto_id)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    """, (tarea._titulo, tarea._descripcion, tarea._fecha de creacion, tarea._fechalimite, tarea._prioridad, tarea._estado, tarea._proyecto_id))
+        
+
+
+    tarea._id = cursor.lastrowid
+    conn.commit()
+    conn.close()
+    return tarea
