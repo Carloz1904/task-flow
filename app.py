c654e42a0db8619165e790bdf0cff9654c2e98eb
CarloZ <carloz190405@gmail.com>
2025-11-03 18:34:22 -0600

    add /crear route

--- app.py
+++ app.py
@@ -16,3 +16,42 @@ def index():
     return render_template('index.html',
                             tareas=tareas_pendientes,
                             proyectos=proyectos)
+
+
+@app.route('/crear', methods=['GET', 'POST'])
+def crear_tarea_web():
+
+    proyectos = db_manager.obtener_proyectos()
+
+    if request.method == 'POST':
+        titulo = request.form.get('titulo')
+        descripcion = request.form.get('descripcion')
+        limite = request.form.get('fecha_limite')
+        prioridad = request.form.get('prioridad')
+
+        proyecto_id = int(request.form.get('proyectos_id'))
+
+        nueva_tarea = Tarea(
+            titulo=titulo,
+            descripcion=descripcion,
+            fecha_limite=limite,
+            prioridad=prioridad,
+            proyecto_id=proyecto_id
+        )
+
+
+
+        db_manager.crear_tarea(nueva_tarea)
+
+
+        return redirect(url_for('index'))
+
+    
+    return render_template('formulario_tarea.html', proyectos=proyectos)
+
+
+if __name__ == 'main':
+
+    db_manager.crear_tabals()
+    print("Iniciando servidor")
+    app.run(debug=True)
